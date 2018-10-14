2ba71c7e513c58a7982b0eee51d4e1b538af4c52
S0016510923 <kirill.esipov@masterdata.ru>
2018-10-14 19:54:38 +0000

    Commit from Web IDE

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.simple.SimpleApp.controller.View1", {
+      
+      toPage2: function(){
+      	sap.ui.getCore().byFieldGroupId("idAppControl").to("page2");
+      }
+	});
+});
